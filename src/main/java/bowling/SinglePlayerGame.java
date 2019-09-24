084f5d79eb451ff5e9aa14765e00d8b01f120672
Hugo-Povereau <47350249+Hugo-Povereau@users.noreply.github.com>
2019-09-24 15:02:16 +0200

    Update SinglePlayerGame.java

--- src/main/java/bowling/SinglePlayerGame.java
+++ src/main/java/bowling/SinglePlayerGame.java
@@ -7,20 +7,83 @@ package bowling;
  */
 public class SinglePlayerGame {
 
-	/**
-	 * Constructeur
-	 */
+	private int score;
+	private int lancer;
+        private int nblancer;
+        private int previous;
+        private int multiplicateur;
+        
 	public SinglePlayerGame() {
-	}
+            lancer = 0;
+            score = 0;
+            nblancer = 0;
+            previous = 0;
+            multiplicateur = 0;
+        }
 
 	/**
-	 * Cette méthode doit être appelée à chaque lancé de boule
+	 * C ette méthode doit être appelée à chaque lancé de boule
 	 *
 	 * @param nombreDeQuillesAbattues le nombre de quilles abattues lors de
 	 * ce lancé
 	 */
 	public void lancer(int nombreDeQuillesAbattues) {
-
+              lancer = nombreDeQuillesAbattues;
+              if(lancer == 10 && nblancer<20){
+                  if (multiplicateur>0){
+                      if (multiplicateur == 2){
+                          score += lancer*3;
+                      }
+                      else{
+                          score += lancer*2;
+                      }
+                      nblancer +=2;
+                      multiplicateur = 2;
+                  }
+                  else{
+                  multiplicateur = 2;
+                  score += lancer;
+                  nblancer +=2;
+                  }
+                  
+              }
+              else if(lancer == 10 && nblancer>=20){
+                  score += lancer;
+              }
+              else{
+                  
+                  if (nblancer%2 ==0){
+                      previous = lancer;
+                      if (multiplicateur>0){
+                          multiplicateur -= 1;
+                          score += lancer*2;
+                      }
+                      else{
+                          score += lancer;
+                      }
+                     
+                  }
+                  else{
+                      if (multiplicateur>0){
+                          if(lancer+previous == 10){
+                          }
+                          else{
+                              multiplicateur -= 1;
+                          }
+                          
+                          score += lancer*2;
+                      }
+                      else{
+                          if(lancer+previous == 10){
+                            multiplicateur = 1;
+                          }
+                          score += lancer;
+                      }
+                      
+                  }
+              nblancer +=1;    
+              }
+              
 	}
 
 	/**
@@ -29,6 +92,6 @@ public class SinglePlayerGame {
 	 * @return Le score du joueur
 	 */
 	public int score() {
-		throw new UnsupportedOperationException("Pas encore implémenté");
-	}
+                return score;
+        }
 }
